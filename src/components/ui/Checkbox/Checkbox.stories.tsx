8f758d600f25919b531164d40360e91fd5703c2d
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-05-01 17:55:58 +0900

    feat(ui): add Checkbox component
    - Added `Checkbox` component using `@radix-ui/react-checkbox` for customizable UI.
    - Added storybook stories for different Checkbox states: Default, Checked, Disabled, DisabledChecked, and AllStates.
    - Updated dependencies in `package.json` and `pnpm-lock.yaml` to include `@radix-ui/react-checkbox`.

---
+++ src/components/ui/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,108 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox } from './Checkbox';
+
+const meta = {
+  title: 'UI/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    checked: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    onCheckedChange: { action: 'checked changed' },
+  },
+} as Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const DisabledChecked: Story = {
+  args: {
+    disabled: true,
+    checked: true,
+  },
+};
+
+// 모든 상태를 한 번에 보여주는 스토리
+export const AllStates: Story = {
+  render: () => (
+    <div className='flex flex-col gap-8'>
+      <div className='flex flex-col gap-4'>
+        <h3 className='text-heading-1 font-semibold text-neutral-500'>
+          Unchecked
+        </h3>
+        <div className='flex items-center gap-4'>
+          <Checkbox id='unchecked' />
+          <label
+            htmlFor='unchecked'
+            className='text-body-2 font-medium text-neutral-900'
+          >
+            체크박스
+          </label>
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-4'>
+        <h3 className='text-heading-1 font-semibold text-neutral-500'>
+          Checked
+        </h3>
+        <div className='flex items-center gap-4'>
+          <Checkbox id='checked' checked />
+          <label
+            htmlFor='checked'
+            className='text-body-2 font-medium text-neutral-900'
+          >
+            체크박스
+          </label>
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-4'>
+        <h3 className='text-heading-1 font-semibold text-neutral-500'>
+          Disabled
+        </h3>
+        <div className='flex flex-col gap-2'>
+          <div className='flex items-center gap-4'>
+            <Checkbox id='disabled' disabled />
+            <label
+              htmlFor='disabled'
+              className='text-body-2 font-medium text-neutral-900'
+            >
+              체크박스
+            </label>
+          </div>
+          <div className='flex items-center gap-4'>
+            <Checkbox id='disabled-checked' disabled checked />
+            <label
+              htmlFor='disabled-checked'
+              className='text-body-2 font-medium text-neutral-900'
+            >
+              체크박스
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  ),
+};
---
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Check } from 'lucide-react';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      'peer h-[18px] w-[18px] shrink-0 rounded-[5px] border-[1.5px] border-neutral-100 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-[0.43] data-[state=checked]:border-primary-500 data-[state=checked]:bg-primary-500 data-[state=checked]:text-neutral-900',
+      className
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn('flex items-center justify-center text-current')}
+    >
+      <Check className='h-4 w-4' />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
