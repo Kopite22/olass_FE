5ec20e45c80132b2a73fc48cdc97ad11f4c5525d
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-05-17 20:57:54 +0900

    feat(slider): salary input ui implement

--- src/_pages/form/steps/YearStep/YearStep.tsx
+++ src/_pages/form/steps/YearStep/YearStep.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/common/Button';
-import { useStep } from '@/components/steps';
+import { LabeledSlider } from '@/components/common/Slider';
 
 import FormBody from '@/_pages/form/components/FormBody';
 import { FormContainer } from '@/_pages/form/components/FormContainer';
@@ -10,34 +10,29 @@ import FormFooter from '@/_pages/form/components/FormFooter';
 import FormHeader from '@/_pages/form/components/FormHeader';
 import FormTitle from '@/_pages/form/components/FormTitle';
 import { useForm } from '@/_pages/form/providers/FormProvider';
-import JobInputDropdown from '@/_pages/form/steps/JobStep/JobInputDropdown';
 
-export default function JobStep() {
-  const { formData, setFormData } = useForm();
-  const { next } = useStep();
-
-  const handleContinue = () => {
-    next();
-  };
+export default function YearStep() {
+  const { formData } = useForm();
 
   return (
     <FormContainer>
       <FormHeader>
-        <FormTitle>어떤 일을 하시나요?</FormTitle>
-        <FormDescription>직업을 적어주세요</FormDescription>
+        <FormTitle>일한 지 얼마나 됐나요?</FormTitle>
+        <FormDescription>연차를 알려주세요</FormDescription>
       </FormHeader>
       <FormBody>
-        <JobInputDropdown
-          selectedJob={formData.job}
-          onSelect={(job) => setFormData({ ...formData, job })}
+        <LabeledSlider
+          heading='신입'
+          min={0}
+          max={10}
+          step={1}
+          startLabel='신입'
+          middleLabel='5년차'
+          endLabel='10년차'
         />
       </FormBody>
       <FormFooter>
-        <Button
-          onClick={handleContinue}
-          disabled={formData.job === null}
-          isFullWidth
-        >
+        <Button disabled={formData.job === null} isFullWidth>
           계속하기
         </Button>
       </FormFooter>
