cacca2e86b7a98f01adef76b7e708891ffadcda9
Kopite22 <125742382+Kopite22@users.noreply.github.com>
2025-07-14 16:27:34 +0900

    Update LandingText.tsx (#4)

--- src/_pages/landing/components/LandingText.tsx
+++ src/_pages/landing/components/LandingText.tsx
@@ -2,7 +2,7 @@ export default function LandingText() {
   return (
     <div className='flex flex-col items-center gap-3 w-full'>
       <h1 className='text-title-2 font-bold text-neutral-800 text-center'>
-        배포 테스트 나랑 비슷한 사람들은
+        나랑 비슷한 사람들은
         <br />
         얼마나 벌까?
       </h1>
