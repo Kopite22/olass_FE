0285e3ea10d98efb9308668270a9de557f94cd58
Kopite22 <nulee1000@gmail.com>
2025-08-03 03:23:42 +0900

    feat: add Summary Component

---
+++ src/_pages/asset-analysis/components/Summary/Mid.tsx
@@ -0,0 +1,14 @@
+const SummaryMid = () => {
+  return (
+    <div className='flex flex-col w-full h-25 py-5 px-10 rounded-3xl text-center mt-4 border-1 border-neutral-50 shadow-[0px_0px_12px_0px_#4DAF0229]'>
+      <span className='text-[20px] font-bold'>
+        지금은 <span className='text-success'>그랜저도 꿈이 아니에요</span>
+      </span>
+      <span className='text-[20px] font-bold'>
+        다만 지금부터 계획이 달라져야 해요
+      </span>
+    </div>
+  );
+};
+
+export default SummaryMid;
