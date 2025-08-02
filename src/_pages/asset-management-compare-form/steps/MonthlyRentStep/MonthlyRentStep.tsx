9e91151f46310fe08d7f0f2b69769adf8c2bf3f7
Kopite22 <nulee1000@gmail.com>
2025-08-03 02:42:02 +0900

    feat: add URLSerachParams

--- src/_pages/asset-management-compare-form/steps/MonthlyRentStep/MonthlyRentStep.tsx
+++ src/_pages/asset-management-compare-form/steps/MonthlyRentStep/MonthlyRentStep.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { cn } from '@/lib/className';
 
@@ -25,6 +26,16 @@ export default function MonthlyRentStep() {
     setSelectedOption(hasMonthlyRent);
     setFormData({ monthlyRent: hasMonthlyRent });
   };
+
+  const uniqueId = uuidv4();
+
+  const searchParams = new URLSearchParams({
+    unique_id: uniqueId,
+    age: formData.age?.toString() ?? '',
+    hasCar: formData.carOwnership?.toString() ?? '',
+    isMonthlyRent: formData.monthlyRent?.toString() ?? '',
+    saveRate: formData.savingsRate?.toString() ?? '',
+  });
 
   return (
     <FormContainer>
@@ -58,7 +69,7 @@ export default function MonthlyRentStep() {
               isFullWidth: true,
             })
           )}
-          href='/asset-analysis'
+          href={`/asset-analysis?${searchParams.toString()}`}
         >
           <div className='flex items-center justify-center gap-1.5'>
             계속하기
