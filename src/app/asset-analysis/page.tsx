fb541d9290072b1315c891046e496f434857da11
Kopite22 <nulee1000@gmail.com>
2025-08-03 16:08:32 +0900

    feat: add asset-analysis

--- src/app/asset-analysis/page.tsx
+++ src/app/asset-analysis/page.tsx
@@ -1,7 +1,36 @@
 import AssetAnalysisPage from '@/_pages/asset-analysis/AssetAnalysisPage';
+import { ConsumptionGradeParams } from '@/apis/asset/postConsumptionGrade';
 
-const Page = () => {
-  return <AssetAnalysisPage />;
+interface PageProps {
+  searchParams: {
+    unique_id?: string;
+    age?: string;
+    hasCar?: string;
+    isMonthlyRent?: string;
+    saveRate?: string;
+  };
+}
+
+const Page = ({ searchParams }: PageProps) => {
+  if (
+    !searchParams.unique_id ||
+    !searchParams.age ||
+    !searchParams.hasCar ||
+    !searchParams.isMonthlyRent ||
+    !searchParams.saveRate
+  ) {
+    throw new Error('필수 파라미터가 누락되었습니다.');
+  }
+
+  const requestBody: ConsumptionGradeParams = {
+    uniqueId: searchParams.unique_id,
+    age: parseInt(searchParams.age, 10),
+    hasCar: searchParams.hasCar === 'true',
+    isMonthlyRent: searchParams.isMonthlyRent === 'true',
+    saveRate: parseInt(searchParams.saveRate, 10),
+  };
+
+  return <AssetAnalysisPage {...requestBody} />;
 };
 
 export default Page;
