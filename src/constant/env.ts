3e07a3011f4d42fbff3b7895b7df5d2533f616d4
Kopite22 <125742382+Kopite22@users.noreply.github.com>
2025-08-18 14:51:38 +0900

    Update env.ts

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -5,5 +5,11 @@ export const showLogger = isLocal
   ? true
   : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true';
 
-export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
-export const GOOGLE_TAG_MANGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
+export const GA_MEASUREMENT_ID =
+  (isProd
+    ? process.env.GA_MEASUREMENT_ID
+    : process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID) || '';
+export const GOOGLE_TAG_MANGER_ID =
+  (isProd
+    ? process.env.GOOGLE_TAG_MANAGER_ID
+    : process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID) || '';
